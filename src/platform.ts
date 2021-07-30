50b08dba77e573d7005dd8ff62e26765221cbab0
null <2584456014@qq.com>
2021-07-30 17:51:17 +0800

    [fix] kill subprocess when kill a process

--- src/platform.ts
+++ src/platform.ts
@@ -1,11 +1,15 @@
 import * as child_process from 'child_process';
 
 /**
- * kill windows process
+ * kill process
 */
 export function kill(pid: number): Error | undefined {
     try {
-        child_process.execSync(`taskkill /PID ${pid} /F`);
+        if (os.platform() == 'win32') {
+            child_process.execSync(`taskkill /PID ${pid} /T /F`);
+        } else {
+            child_process.execSync(`kill -9 ${pid}`);
+        }
     } catch (error) {
         return error;
     }
